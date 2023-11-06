387155618402a394466a936c7ef6aa099e4f56e3
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-06 13:30:33 +0100

    feat: add ClassWithDifficultCtorToMock example

---
+++ Sandbox/Sandbox/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using System.Runtime.Serialization;
+global using FluentAssertions;
+global using NUnit.Framework;
---
+++ Sandbox/Sandbox/MockCtorExample/MockCtor.cs
@@ -0,0 +1,26 @@
+﻿namespace Sandbox.MockCtorExample;
+
+public class MockCtor
+{
+    [Test]
+    public void demonstrate_ctor_creation()
+    {
+        var cls = Create();
+        cls.Dep1 = "test";
+
+        cls.Dep1.Should().Be("test");
+    }
+    
+    private static ClassWithDifficultCtorToMock Create() =>
+        (ClassWithDifficultCtorToMock)FormatterServices.GetUninitializedObject(typeof(ClassWithDifficultCtorToMock));
+}
+
+public class ClassWithDifficultCtorToMock
+{
+    private ClassWithDifficultCtorToMock(string dep1)
+    {
+        Dep1 = dep1;
+    }
+    
+    public string Dep1 { get; set; }
+}
